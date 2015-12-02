7f9951dc4c5a57d263fae5c9c5be10428d518eca
Carl Winkler <carl@carlossus.com>
2015-12-02 12:57:58 +0800

    Convert gulpfile to typescript for es5 support

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
+
 throw new SnarkError('Gulp encountered');
-gulp.task('minify-css', function () {
+
+gulp.task('minify-css', () => {
     return gulp.src('src/assets/css/*.css')
         .pipe(minifyCss({}))
         .pipe(gulp.dest('assets/css/styles'));
 });
+
 gulp.task('default', [
     'minify-css'
 ]);
+
 function SnarkError(message) {
     this.name = 'SnarkError';
     this.message = message || 'Undefined SnarkError encountered';
