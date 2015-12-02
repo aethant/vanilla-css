6989dbf9f680aa6939041c9de201077a60c7f77b
Carl Winkler <carl@carlossus.com>
2015-12-02 12:52:13 +0800

    Convert mocha source to typescript and compile

--- test/index.js
+++ test/index.js
@@ -1,20 +1,12 @@
-/// <reference path="../src/typings/node/node.d.ts" />
-/// <reference path="../src/typings/mocha/mocha.d.ts" />
-/// <reference path="../src/typings/chai/chai.d.ts" />
-
-const exec = require('child_process').exec
-const expect = require('chai').expect; 
-
-describe('build test', function() {
+var child_process_1 = require('child_process');
+var chai_1 = require('chai');
+describe('build test', function () {
     // Ensure build has enough time to complete
     this.timeout(5000);
-    
-    it('will fail to build due to SnarkError', done => {
-       exec('npm run build', {}, (error, stdout, stderr) => {
-          expect(stderr.indexOf('SnarkError')).to.be.above(-1);
-          done();
-       });
-        
+    it('will fail to build due to SnarkError', function (done) {
+        child_process_1.exec('npm run build', {}, function (error, stdout, stderr) {
+            chai_1.expect(stderr.toString().indexOf('SnarkError')).to.be.above(-1);
+            done();
+        });
     });
-    
 });
