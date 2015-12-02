72302fc9d4a3ed4faf3b7cd3f975ce8b246ca8d7
Carl <carl@carlossus.com>
2015-12-02 22:43:51 +0800

    Add check in exhaustive unit tests to check that the build failed and increase timer

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,18 @@
 import { exec } from 'child_process';
-import { expect } from 'chai'; 
+import { expect } from 'chai';
 
 describe('build test', function() {
     // Ensure build has enough time to complete
-    this.timeout(5000);
-    
+    this.timeout(10000);
+
     it('will fail to build due to SnarkError', done => {
-       exec('npm run build', {}, (error, stdout, stderr) => {
-          expect(stderr.toString().indexOf('SnarkError')).to.be.above(-1);
-          done();
-       });
-        
+        exec('npm run build', {}, (error, stdout, stderr) => {
+            var concatOutput = stdout.toString() + stderr.toString();
+            expect(error).to.exist;
+            expect(concatOutput.indexOf('SnarkError')).to.be.above(-1);
+            done();
+        });
+
     });
-    
+
 });
